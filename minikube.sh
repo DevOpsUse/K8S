bbb8d6a0dca6202c9cf33603ef881aeb5798dc37
SVN Reddy <venkatasykam@yahoo.com>
2021-06-24 20:44:30 +0530

    Create minikube.sh

---
+++ minikube.sh
@@ -0,0 +1,9 @@
+
+
+apt install virtualbox virtualbox-ext-pack -y
+
+wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+chmod +x minikube-linux-amd64
+
+sudo mv minikube-linux-amd64 /usr/local/bin/minikube
